97df7ce157970cda95cd65bf1c6daa4603ce9e23
Huy Phan <huy.phan@go1.com>
2019-10-02 14:36:53 +0700

    Add script to get environments from aws services

---
+++ aws-get-service-env.sh
@@ -0,0 +1,20 @@
+aws configure set region $(aws configure get region)
+aws configure set aws_access_key_id $(aws configure get aws_access_key_id)
+aws configure set aws_secret_access_key $(aws configure get aws_secret_access_key)
+
+function getEnv
+{
+    while test $# -gt 0
+    do
+        echo "Get environments from service: $1"
+        taskDefinition=$(aws ecs describe-services --cluster production --services $1 | jq -c '.services[].deployments[].taskDefinition' | sed 's/"//g')
+        aws ecs describe-task-definition --task-definition $taskDefinition | jq '.taskDefinition.containerDefinitions[].environment'
+        shift
+    done
+}
+
+# Get list of AWS services and parse line by line
+service_list=$(aws ecs list-services --cluster production | jq -c '.[][]' | sed 's/"//g')
+
+# Get envs from service list
+getEnv $service_list
--- aws-get-task-env.sh
+++
@@ -1,8 +0,0 @@
-function my_function
-{
-    while test $# -gt 0
-    do
-        echo "do something with $1"
-        shift
-    done
-}
